38289c0cbc7fc331f14baac3b6295b342b69cb24
Vincent Guzman <vguzman812@gmail.com>
2023-12-13 21:52:27 +0000

    WHOO some tests are passing now.

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,12 +1,86 @@
 package com.example.controller;
 
+import com.example.entity.Account;
+import com.example.entity.Message;
+import com.example.service.AccountService;
+import com.example.service.MessageService;
 
-/**
- * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
- * found in readme.md as well as the test cases. You be required to use the @GET/POST/PUT/DELETE/etc Mapping annotations
- * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
- * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
- */
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/")
 public class SocialMediaController {
+
+    @Autowired
+    private AccountService accountService;
+
+    @Autowired
+    private MessageService messageService;
+
+    @PostMapping("/accounts/register")
+    public ResponseEntity<Account> register(@RequestBody Account account) {
+        if (accountService.isValidRegistrationInput(account)) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        if (accountService.findAccountByUsername(account.getUsername()).isPresent()) {
+            return ResponseEntity.status(409).build();
+        }
+
+        Account savedAccount = accountService.saveAccount(account);
+        return ResponseEntity.ok(savedAccount);
+    }
+
+    @PostMapping("/accounts/login")
+    public ResponseEntity<Account> login(@RequestBody Account account) {
+        return accountService.findAccountByUsername(account.getUsername())
+                .filter(a -> a.getPassword().equals(account.getPassword()))
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.status(401).build());
+    }
+
+    @PostMapping("/messages")
+    public ResponseEntity<Message> createMessage(@RequestBody Message message) {
+        if (messageService.isValidMessageText(message)) {
+            return ResponseEntity.badRequest().build();
+        }
+        Message savedMessage = messageService.saveAccount(message);
+        return ResponseEntity.ok(savedMessage);
+    }
+
+    @GetMapping("/messages")
+    public ResponseEntity<List<Message>> getAllMessages() {
+        List<Message> messages = messageService.findAll();
+        return ResponseEntity.ok(messages);
+    }
+
+    @GetMapping("/messages/{id}")
+    public ResponseEntity<Message> getMessageById(@PathVariable Integer id) {
+        return messageService.findmessageById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.ok().build());
+    }
+
+    @DeleteMapping("/messages/{id}")
+    public ResponseEntity<Integer> deleteMessage(@PathVariable Integer id) {
+        int result = messageService.deleteMessage(id);
+        return result == 1 ? ResponseEntity.ok(result) : ResponseEntity.ok().build();
+    }
+
+    @PatchMapping("/messages/{id}")
+    public ResponseEntity<Integer> updateMessage(@PathVariable Integer id, @RequestBody Message message) {
+        int result = messageService.updateMessageText(id, message.getMessage_text());
+        return result == 1 ? ResponseEntity.ok(result) : ResponseEntity.badRequest().build();
+    }
+
+    @GetMapping("/accounts/{accountId}/messages")
+    public ResponseEntity<List<Message>> getMessagesByUser(@PathVariable Integer accountId) {
+        List<Message> messages = messageService.findMessageByPostedBy(accountId);
+        return ResponseEntity.ok(messages);
+    }
 
 }
--- src/main/java/com/example/repository/MessageRepository.java
+++ src/main/java/com/example/repository/MessageRepository.java
@@ -1,19 +1,20 @@
 package com.example.repository;
 
 import com.example.entity.Message;
-
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Integer> {
+
     /**
-     * Find all messages posted by a specific user.
+     * Find all messages posted by a specific user using a custom query.
+     * 
      * @param id The ID of the user who posted the messages.
      * @return A list of messages.
      */
+    @Query("SELECT m FROM Message m WHERE m.posted_by = :id")
     List<Message> findByPostedBy(Integer id);
-
 }
